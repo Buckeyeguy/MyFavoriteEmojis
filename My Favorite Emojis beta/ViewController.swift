c405f742d5981ec536602cd60b594d74708e3cc8
David Gretz <dg@buckeyeguy.com>
2016-10-02 18:05:38 +0700

    DataSource-Delegate-Emoji commit

--- My Favorite Emojis beta/ViewController.swift
+++ My Favorite Emojis beta/ViewController.swift
@@ -8,15 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var coolTableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableview.dataSource=self
+        coolTableview.delegate=self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello, my name is HAL 👁"
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
